b37671fc496c3215adffd105d389572705478294
BerkayBaran <berkaybara@gmail.com>
2021-07-09 06:28:34 +0300

    Commit

---
+++ MVCKutuphane/MVCKutuphane/Controllers/Admin Controller/UyeController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCKutuphane.Models.Entity;
+namespace MVCKutuphane.Controllers
+{
+    public class UyeController : Controller
+    {
+        // GET: Uye
+        DBKutuphaneEntities db = new DBKutuphaneEntities();
+        public ActionResult Index(string a)
+        {
+            var uye = from u in db.TBL_Uyeler select u;
+            if (!string.IsNullOrEmpty(a))
+            {
+                uye = uye.Where(x => x.Uye_Adi.Contains(a) && x.Tur==1 );
+            }
+            return View(uye.ToList());
+        }
+        [HttpGet]
+        public ActionResult UyeEkle()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult UyeEkle(TBL_Uyeler u)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("UyeEkle");
+            }
+            if (db.TBL_Uyeler.Any(x => x.Kullanici_Adi == u.Kullanici_Adi || x.Uye_Mail == u.Uye_Mail))
+            {
+                ViewBag.Hata = "Kullanici Adi veya Email Sistemde Mevcut.";
+                return View("UyeEkle", u);
+            }
+            else
+            {
+                if (u.Cinsiyet == "E" || u.Cinsiyet == "K" || u.Cinsiyet == "B")
+                {
+                    u.OduncDurum = true;
+                    u.Bakiye = (decimal)0.0000;
+                    db.TBL_Uyeler.Add(u);
+                    db.SaveChanges();
+                    ViewBag.BasariliKayit = "Kayıt Olma Basarili!";
+                    return View();
+                }
+                else
+                {
+                    u.OduncDurum = true;
+                    u.Bakiye = (decimal)0.0000;
+                    u.Cinsiyet = "B";
+                    db.TBL_Uyeler.Add(u);
+                    db.SaveChanges();
+                    ViewBag.BasariliKayit = "Kayıt Olma Basarili!";
+                    return View();
+                }
+            }
+                
+        }
+        public ActionResult UyeSil(int id)
+        {
+            var uye = db.TBL_Uyeler.Find(id);
+            uye.Durum=false;
+            uye.OduncDurum = false;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult UyeGetir(int id)
+        {
+            var uye = db.TBL_Uyeler.Find(id);
+            return View("UyeGetir", uye);
+        }
+        public ActionResult UyeGuncelle(TBL_Uyeler u)
+        {
+            var uye = db.TBL_Uyeler.Find(u.ID);
+            if (uye.OduncDurum == false)
+            {
+                uye.Uye_Adi = u.Uye_Adi;
+                uye.Uye_Soyadi = u.Uye_Soyadi;
+                uye.Uye_Mail = u.Uye_Mail;
+                uye.Kullanici_Adi = u.Kullanici_Adi;
+                uye.Sifre = u.Sifre;
+                uye.Durum = true;
+                uye.OduncDurum = false;
+                uye.Tur = 1;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                uye.Uye_Adi = u.Uye_Adi;
+                uye.Uye_Soyadi = u.Uye_Soyadi;
+                uye.Uye_Mail = u.Uye_Mail;
+                uye.Kullanici_Adi = u.Kullanici_Adi;
+                uye.Sifre = u.Sifre;
+                uye.Durum = true;
+                uye.OduncDurum = true;
+                uye.Tur = 1;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+           
+        }
+    }
+}
---
+++ MVCKutuphane/MVCKutuphane/Controllers/Admin Controller/YazarController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCKutuphane.Models.Entity;
+
+namespace MVCKutuphane.Controllers
+{
+    public class YazarController : Controller
+    {
+        // GET: Yazar
+        DBKutuphaneEntities db = new DBKutuphaneEntities();
+        public ActionResult Index(string y)
+        {
+            var yazarlar = from k in db.TBL_Yazar select k;
+            if (!string.IsNullOrEmpty(y))
+            {
+                yazarlar = yazarlar.Where(x => x.Yazar_Adi.Contains(y));
+            }
+            return View(yazarlar.ToList());
+        }
+        [HttpGet]
+        public ActionResult YazarEkle()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YazarEkle(TBL_Yazar d)
+        {
+            db.TBL_Yazar.Add(d);
+            db.SaveChanges();
+            return View();
+        }
+        public ActionResult YazarSil(int id)
+        {
+            var yazarsil = db.TBL_Yazar.Find(id);
+            var ktp = db.TBL_Kitaplar.Where(a => a.Yazar_ID == id).FirstOrDefault();
+            if (ktp != null)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                db.TBL_Yazar.Remove(yazarsil);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+        }
+        public ActionResult YazarGetir(int id)
+        {
+            var yazargetir = db.TBL_Yazar.Find(id);
+            return View("YazarGetir", yazargetir);
+        }
+        public ActionResult YazarGuncelle(TBL_Yazar y)
+        {
+            var yzr = db.TBL_Yazar.Find(y.ID);
+            yzr.Yazar_Adi = y.Yazar_Adi;
+            yzr.Yazar_Soyad = y.Yazar_Soyad;
+            yzr.Kitap_Sayisi = y.Kitap_Sayisi;
+            yzr.Detay = y.Detay;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
